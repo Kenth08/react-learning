20c686a531d4e662d1ad09a76d8650c32b38eac3
Kenth08 <kenthrepollo@gmail.com>
2026-06-07 23:42:38 +0800

    feat: implement routing with LandingPage and LoginPage components, remove unused CSS files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LandingPage from './LandingPage'
 import LoginPage from './LoginPage'
 
 export default function App() {
-  return <LoginPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/landingpage" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
