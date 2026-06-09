033d02f5ec0e4ff124b1ef0ab096db37692b980d
Kenth08 <kenthrepollo@gmail.com>
2026-06-10 07:41:53 +0800

    add a path in shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import Shop from "./pages/shop"; // Matches your lowercase filename 'shop.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Root Path: Loads your premium sneaker carousel directly */}
         <Route path="/" element={<LandingPage />} />
+        
+        {/* Authentication Path */}
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/landingpage" element={<Navigate to="/" replace />} />
+        
+        {/* Product Catalogue Path */}
+        <Route path="/shop" element={<Shop />} />
+
+        {/* Fallback Catch: Redirects any broken links back to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
